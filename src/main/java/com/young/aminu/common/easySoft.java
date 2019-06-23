1437f714dde3f865d16d97c2c92a5ad8ca20d62a
yang <youngeasylife@163.com>
2019-06-23 21:45:19 +0800

    冒泡

---
+++ src/main/java/com/young/aminu/common/easySoft.java
@@ -0,0 +1,35 @@
+package com.young.aminu.common;
+
+/**
+ * created with IntelliJ IDEA.
+ * author: yang
+ * date: 2019/6/23
+ * time: 21:20
+ * desc:
+ */
+public class easySoft {
+    public static void main(String[] args) {
+        int[] a = new int[]{11, 3, 123, 21321, 12, 1, 2, 434, 21, 3};
+        easySort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    /**
+     * desc: 冒泡排序
+     *
+     * @param sort :
+     */
+    private static void easySort(int[] sort) {
+        for (int i = 0; i < sort.length; i++) {
+            for (int j = sort.length - 1; j > i; j--) {
+                int temp = sort[i];
+                if (temp > sort[j]) {
+                    sort[i] = sort[j];
+                    sort[j] = temp;
+                }
+            }
+        }
+    }
+}
